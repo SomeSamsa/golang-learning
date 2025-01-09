3eb15d587fb25117a14da55e4fb0156185eb8544
SomeSamsa <alina.abasova02@gmail.com>
2025-01-10 00:00:06 +0600

    feat: some more basics

---
+++ practice/basics/variables/var.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var c, java bool
+var smd = "some"
+var to_ = 9
+var python = true
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java, smd, to_)
+}
---
+++ practice/basics/variables/var3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+//some := "some"
+var some = "some"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(some, i, j, k, c, python, java)
+}
